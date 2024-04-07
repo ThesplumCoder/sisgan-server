263d66bd26af0f0c83e62fb58a5cfd0e6a70babe
Anderson Acuña <andersonyacvpers002360@outlook.com>
2024-04-06 19:57:30 -0500

    Archivos generados por el inicializador.

---
+++ src/main/java/com/uis/sisgan/SisganApplication.java
@@ -0,0 +1,13 @@
+package com.uis.sisgan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisganApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisganApplication.class, args);
+	}
+
+}
