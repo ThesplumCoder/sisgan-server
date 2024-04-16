75b3f66d65c78747d0f7b3f18eec389edd8720b5
SorryIWinxX <daniel2191937@correo.uis.edu.co>
2024-04-16 11:18:31 -0500

    Objects of User, Farmer, Lots, Catter and Driver. Just Getter and Setters

---
+++ src/main/java/com/uis/sisgan/Lots.java
@@ -0,0 +1,13 @@
+package com.uis.sisgan;
+
+public class Lots {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
