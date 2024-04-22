18fbe51fd9370237c29c99434df91b5023cc53ba
Anderson Acuña <andersonyacvpers002360@outlook.com>
2024-04-21 22:57:27 -0500

    Código para creación de las tablas terminado.

--- table-creation.sql
+++ table-creation.sql
@@ -4,5 +4,57 @@ CREATE TABLE USERS (
 id INTEGER,
 id_card VARCHAR(20),
 id_driving_livense INTEGER,
-id_ica_official INTEGER
-)
+id_ica_official INTEGER,
+first_name VARCHAR(100),
+last_name VARCHAR(100),
+password VARCHAR(200),
+birth_date DATE,
+mark VARCHAR(100),
+farm_name VARCHAR(100),
+email VARCHAR(100),
+PRIMARY KEY(id)
+);
+
+CREATE TABLE ROLES (
+id INTEGER,
+id_user INTEGER,
+rol VARCHAR(10),
+PRIMARY KEY(id),
+FOREIGN KEY(id_user) REFERENCES USERS(id)
+);
+
+CREATE TABLE CATTLES (
+id INTEGER,
+id_user INTEGER,
+birth_date DATE,
+weigth INTEGER,
+heigth INTEGER,
+sex VARCHAR(1),
+breed VARCHAR(15),
+PRIMARY KEY(id),
+FOREIGN KEY(id_user) REFERENCES USERS(id)
+);
+
+CREATE TABLE LOTS (
+id INTEGER,
+id_cattle INTEGER,
+name VARCHAR(80) NOT NULL,
+PRIMARY KEY(id),
+FOREIGN KEY(id_cattle) REFERENCES CATTLES(id)
+);
+
+CREATE TABLE INTERNAL_MOVEMENT_GUIDES (
+id INTEGER,
+id_lot INTEGER,
+vehicle_plate VARCHAR(10),
+origin VARCHAR(100),
+destination VARCHAR(100),
+signature_petitioner VARCHAR(100),
+id_transporter INTEGER,
+application_date DATE,
+mobilization_date DATE,
+signature_ica_official VARCHAR(100),
+PRIMARY KEY(id),
+FOREIGN KEY(id_lot) REFERENCES LOTS(id),
+FOREIGN KEY(id_transporter) REFERENCES USERS(id)
+);
