af0f6ee56ed9a92607096df763103219774965b8
SorryIWinxX <104944988+SorryIWinxX@users.noreply.github.com>
2024-06-07 11:02:37 -0500

    Cambios en la entidad y controlador de las guías de movimiento interno.

---
+++ src/main/java/com/uis/sisgan/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.uis.sisgan.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/home")
+public class HomeController {
+
+
+    public String HomeController(){
+
+
+        return "/home/propietary";
+    }
+
+
+
+
+
+
+}
