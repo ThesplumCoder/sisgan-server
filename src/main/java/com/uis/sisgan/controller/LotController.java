37ce5339ffea2322815753bdcccaefd79d140969
SorryIWinxX <daniel2191937@correo.uis.edu.co>
2024-06-24 08:05:09 -0500

    CorsFixed

--- src/main/java/com/uis/sisgan/controller/LotController.java
+++ src/main/java/com/uis/sisgan/controller/LotController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Base64;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RequestMapping("/lots")
 @RestController
 public class LotController {
